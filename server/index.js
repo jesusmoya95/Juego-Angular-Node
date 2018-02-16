642be4e6f368e69a76931b0464bceffac112f7b8
jesusmoya95 <jesusmoyaguevara@hotmail.com>
2018-02-16 13:13:56 +0100

    subir juego, tenia un error8

--- server/index.js
+++ server/index.js
@@ -200,5 +200,5 @@ io.on('connection', function(socket){
   puerto =  process.env.PORT || 3000;
   /*Puerto por el que se conecta al servidor*/
   http.listen(puerto, function(){
-    console.log('listening on *:3000');
+    console.log('listening on *:'+puerto);
   });
